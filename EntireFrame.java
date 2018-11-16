1859c0bf40d5e46da709f669bcf5d3e1f2f4510a
Mission Marcus <missionmarcus@gmail.com>
2018-11-16 14:22:05 -0600

    Updated frame title.

--- EntireFrame.java
+++ EntireFrame.java
@@ -13,7 +13,7 @@ public class EntireFrame{
 	
 	public static void main(String [] args)
 	{
-		JFrame newFrame = new JFrame("BestBuy is the best."); // make the Jframe
+		JFrame newFrame = new JFrame("Hello"); // make the Jframe
 		newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close on close
 		newFrame.setVisible(true);
 		newFrame.setSize(500, 500); // set the size to 500 by 500.
